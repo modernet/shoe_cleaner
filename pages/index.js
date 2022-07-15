44cc05d235948a7937a048d0a6ea70d74a2ed179
Marco Bejarano <marco.bejarano03@gmail.com>
2022-07-14 21:01:02 -0700

    finished with iframe again

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,23 @@ import Hero from '../components/Hero';
 import ProductSection from '../components/ProductSection';
 import { CssBaseline } from '@mui/material';
 import PackageSection from '../components/PackageSection';
-import { client } from '../lib/client';
 import Footer from '../components/Footer';
 
 
-export default function Home({products}) {
+export default function Home() {
   return (
     <div>
-      <CssBaseline />
+      <CssBaseline>
 
       <Navbar />
       <Hero />
       <ProductSection />
       <PackageSection />
-      {/* <div className="package">
-      {products?.map((product) => <Product key={product._id} product={product} />)}
-      </div> */}
       <Footer />
+
+      </CssBaseline>
     </div>
   )
 }
 
-export const getServerSideProps = async () => {
-  const query = '*[_type == "product"]';
-  const products = await client.fetch(query);
 
-  return {
-    props: { products }
-  }
-};
--- pages/items.js
+++ pages/items.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import { CssBaseline } from '@mui/material';
+import React from 'react';
 import Navbar from '../components/Navbar';
+import { client } from '../lib/client';
+import Grid from '@mui/material/Grid';
+import Product from '../components/Product';
 
-export default function Items() {
+export default function Items({products}) {
   return (
-    <>
+    <div>
+        <CssBaseline>
         <Navbar />
-    </>
+        <Grid container spacing={2} xs={{
+          marginTop : '40px',
+        }} >
+        <div className="package">
+        {products?.map((product) => <Product item xs={3} key={product._id} product={product} />)}
+        </div>
+        </Grid>
+        </CssBaseline>
+    </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  return {
+    props: { products }
+  }
+};
